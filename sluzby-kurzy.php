dc0385e4bb385f30a0910f94198a3ebd71ac659d
Vít Zaoral <Vt Zaoral>
2017-06-04 13:23:47 +0200

    Nahrazení head, keywords, title

--- sluzby-kurzy.php
+++ sluzby-kurzy.php
@@ -1,35 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <meta name="keywords" content="physio, physiotherapy, physiotherapist, sports injuries,HTML5,CSS3,Responsive,HTML Template,Accupuncture,massage,pilate,occupational Health"
-  />
-  <meta name="description" content="Physio plus is clean and simple website template that have a feature like each service have a detail page and Terapist trainer page, location page and also more feature.">
-  <title>Physio Plus | Physiotherapy Responsive Website Template</title>
-
-  <!-- Bootstrap -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <!-- Template Style.css -->
-  <link href="css/style.css" rel="stylesheet">
-
-  <!-- icon css -->
-  <link rel="stylesheet" type="text/css" href="css/flaticon.css">
-  <!-- Template Font -->
-  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
-  <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,500italic,700,700italic,300,300italic' rel='stylesheet'
-    type='text/css'>
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+  <!-- head -->
+  <?php $title = 'Služby - kurzy'; include "templates/head.php" ?>
 </head>
 
 <body>
---
+++ templates/head.php
@@ -0,0 +1,31 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="keywords" content="fyzio, fyzioterapie, sportovní zranění, masáž, cvičení, rehabilitace, Bohuňovice, rpilates, zdraví, pilates, joga"
+/>
+<meta name="description" content="Rpilates - rehabilitační ambulance a sportovní fyzioterapie.">
+<title>Rpilates | <?php echo $title ?></title>
+
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- Template Style.css -->
+<link href="css/style.css" rel="stylesheet">
+
+<!-- Template slider css -->
+<link href="css/owl.theme.css" rel="stylesheet">
+<link href="css/owl.carousel.css" rel="stylesheet">
+<link href="css/animate.css" rel="stylesheet">
+<!-- icon css -->
+<link rel="stylesheet" type="text/css" href="css/flaticon.css">
+<!-- Template Font -->
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
